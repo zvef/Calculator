17327591995230fede506dae107a1eba3bae7c89
zvef <jakekneece7@gmail.com>
2023-05-23 01:04:06 -0400

    CHANGING DIVS TO BUTTONS tunnel vision from the last project

--- main.js
+++ main.js
@@ -1,30 +1,117 @@
-
-
 const operator = '+';
 const numberOne = 20;
 const numberTwo = 40;
 
+const buttonDivs = [...document.querySelectorAll('.buttonRow')];
+let screenDisplay = document.querySelector('.screen');
 
+let displayValue = 0;
 
+//SHOULD HAVE USED <BUTTONS> INSTEAD OF <DIVS> ...
 
+buttonDivs.forEach(buttDiv => {
+    if(buttDiv.id == 'one') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '1';
+            screenDisplay.innerHTML += displayValue;    
+        });
 
+    } else if(buttDiv.id == 'two') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '2';
+            screenDisplay.innerHTML += displayValue;  
+        });
 
+    } else if(buttDiv.id == 'three') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '3';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'four') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '4';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'five') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '5';
+            screenDisplay.innerHTML += displayValue;          
+        });
 
+    } else if(buttDiv.id == 'six') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '6';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'seven') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '7';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'eight') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '8';
+            screenDisplay.innerHTML += displayValue;          
+        });
 
+    } else if(buttDiv.id == 'nine') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '9';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'zero') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '0';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'divide') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '1';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'multiply') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '1';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'subtract') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '1';
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'add') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = 
+            screenDisplay.innerHTML += displayValue;          
+        })
 
+    } else if(buttDiv.id == 'equals') {
+        buttDiv.addEventListener('click', () => {
+            displayValue = '';
+            screenDisplay.innerHTML += displayValue;          
+        })
+
+    } else if(buttDiv.id == 'clear') {
+        buttDiv.addEventListener('click', () => {
+        displayValue += '';
+        screenDisplay.innerHTML += displayValue;          
+        });
+    }
+});
 
 function add(num1, num2) {
     return num1 + num2;
 }
+
 function subtract(num1, num2) {
     if(num2 > num1){
         return num2 - num1;
@@ -32,9 +119,11 @@ function subtract(num1, num2) {
         return num1 - num2;
     }
 }
+
 function multiply(num1, num2) {
     return num1 * num2;
 }
+
 function divide(num1, num2) {
     if(num2 > num1){
         return num2 / num1;
@@ -42,6 +131,7 @@ function divide(num1, num2) {
         return num1 / num2;
     }
 }
+
 function operate(num, op, num2) {
     if(op == '+') {
         return add(num, num2);
@@ -55,6 +145,3 @@ function operate(num, op, num2) {
         console.log('ERROR');
     }
 }
-
-console.log(operate(numberOne, operator, numberTwo));
-
